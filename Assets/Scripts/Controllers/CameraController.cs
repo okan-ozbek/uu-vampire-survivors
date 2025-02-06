80fd765f36bd5826850dfa476d0695998f8f1357
Okan Ozbek <devokan@outlook.com>
2025-02-06 23:59:29 +0100

    New begin

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -4,33 +4,51 @@ namespace Controllers
 {
     public class CameraController : MonoBehaviour
     {
+        
         [SerializeField] private Transform player;
-        [SerializeField] private Vector2 deadzoneSize = new(2f, 2f);
-        [SerializeField] private float smoothSpeed = 0.125f;
 
+        [SerializeField] private Vector2 deadzoneSize;
+        [SerializeField] private float smoothSpeed = 15f;
+        
         private Vector3 _offset;
+        private Vector3 _targetPosition;
 
         private void Start()
         {
             _offset = transform.position - player.position;
         }
-
+        
+        private void FixedUpdate()
+        {
+            _targetPosition = player.position + _offset;
+        }
+        
         private void LateUpdate()
         {
-            Vector3 targetPosition = player.position + _offset;
             Vector3 cameraPosition = transform.position;
-
-            if (Mathf.Abs(targetPosition.x - cameraPosition.x) > deadzoneSize.x)
+        
+            if (Mathf.Abs(_targetPosition.x - cameraPosition.x) > deadzoneSize.x)
             {
-                cameraPosition.x = targetPosition.x - Mathf.Sign(targetPosition.x - cameraPosition.x) * deadzoneSize.x;
+                cameraPosition.x = _targetPosition.x - Mathf.Sign(_targetPosition.x - cameraPosition.x) * deadzoneSize.x;   
             }
-
-            if (Mathf.Abs(targetPosition.y - cameraPosition.y) > deadzoneSize.y)
+        
+            if (Mathf.Abs(_targetPosition.y - cameraPosition.y) > deadzoneSize.y)
             {
-                cameraPosition.y = targetPosition.y - Mathf.Sign(targetPosition.y - cameraPosition.y) * deadzoneSize.y;
+                cameraPosition.y = _targetPosition.y - Mathf.Sign(_targetPosition.y - cameraPosition.y) * deadzoneSize.y;
             }
-
+        
             transform.position = Vector3.Lerp(transform.position, cameraPosition, smoothSpeed * Time.deltaTime);
+        }
+        
+        private void OnDrawGizmos()
+        {
+            if (player == null)
+            {
+                return;
+            }
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, new Vector3(deadzoneSize.x * 2, deadzoneSize.y * 2, 0));
         }
     }
 }
